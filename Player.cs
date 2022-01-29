4bb1f732a3f612043e72bb2c6730a3fcb01308df
AtresAlfahan <martin.sebastian.stasch@googlemail.com>
2022-01-29 13:28:53 +0100

    basic player movement implemented

--- Player.cs
+++ Player.cs
@@ -17,20 +17,70 @@ namespace DreamAwake
 
         private SimpleInputMap<GameAction> _InputMap;
         private Circle _Dot;
-        private float dotRadius = 5f;
+        private float _DotRadius = 5f;
+
+        private Vector2f _Direction;
+
+        private float _MovementSpeed = 100f;
+        private float _JumpForce = 10f;
+
+        private float _GroundLevel = 200;
+
+
 
 
         public Player(Core core, SimpleInputMap<GameAction> inputMap) : base(core)
         {
             _InputMap = inputMap;
-            _Dot = new Circle(_Core, dotRadius, Color.Blue, Color.Yellow);
+            _InputMap.MappedOperationInvoked += HandleInput;
+            
+            _Dot = new Circle(_Core, _DotRadius, Color.Blue, Color.Yellow);
             _Dot.Position = new Vector2f(360, 260);
+
             Add(_Dot);
+
+            
         }
+
+
+        private void HandleInput(GameAction action, bool activate)
+        {
+            switch (action)
+            {
+                case GameAction.Left:
+                    if (activate)
+                        _Direction = new Vector2f(-1, _Direction.Y);
+                    else
+                        _Direction = new Vector2f(0, _Direction.Y);
+                    break;
+                case GameAction.Right:
+                    if (activate)
+                        _Direction = new Vector2f(1, _Direction.Y);
+                    else
+                        _Direction = new Vector2f(0, _Direction.Y);
+                    break;
+                case GameAction.Jump:
+                    if (activate)
+                        _Direction = new Vector2f(_Direction.X, -1);
+                    else
+                        _Direction = new Vector2f(_Direction.X, 1);
+                    break;
+                case GameAction.Activate:
+                    break;
+                default:
+                    break;
+            }
+        }
+
 
         public override void Update(float deltaT)
         {
-            base.Update(deltaT);
+
+            _Dot.Position += _Direction * _MovementSpeed * deltaT;
+
+            if (_Dot.Position.Y >= _GroundLevel)
+                _Dot.Position = new Vector2f(_Dot.Position.X, _GroundLevel);
+
         }
     }
 }
